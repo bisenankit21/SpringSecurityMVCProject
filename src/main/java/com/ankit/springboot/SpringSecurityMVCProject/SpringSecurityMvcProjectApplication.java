0c7e9cc90f0655fd8dd579b153a217a64c51cfec
AnkitBisen <bisenankit21@gmail.com>
2024-06-11 23:09:46 +0530

    Initial commit

---
+++ src/main/java/com/ankit/springboot/SpringSecurityMVCProject/SpringSecurityMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.springboot.SpringSecurityMVCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMvcProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankit/springboot/SpringSecurityMVCProject/SpringSecurityMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.springboot.SpringSecurityMVCProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
